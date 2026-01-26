62412385abb1f446f34f8f3ad629a8a346ae355a
alv <amelia@personal-mac>
2026-01-25 22:39:34 -0800

    got a little crazy for a second

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,8 +1,19 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
-
+use std::ops::Add;
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub struct WrappingU32 {
     value: u32,
+}
+
+impl Add<WrappingU32> for WrappingU32 {
+    type Output = Self;
+
+    fn add(self, rhs: WrappingU32) -> Self::Output {
+        WrappingU32 {
+            value: self.value.wrapping_add(rhs.value),
+        }
+    }
 }
 
 impl WrappingU32 {
