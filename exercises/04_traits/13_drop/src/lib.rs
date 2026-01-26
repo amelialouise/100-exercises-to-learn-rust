8c2463961b63fd443611804229bcb0b500b3fa3b
alv <amelia@personal-mac>
2026-01-25 23:18:35 -0800

    phew!

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -1,7 +1,28 @@
 // TODO: implement a so-called "Drop bomb": a type that panics when dropped
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
+// This is a unit struct, i.e. a struct with no fields. -- nah, defusing needs a field!
+pub struct DropBomb {
+    defused: bool,
+}
 
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if !self.defused {
+            panic!("DropBomb was dropped without being defused!");
+        }
+    }
+}
+
+impl DropBomb {
+    pub fn new() -> DropBomb {
+        DropBomb { defused: false }
+    }
+
+    pub fn defuse(&mut self) {
+        self.defused = true;
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
